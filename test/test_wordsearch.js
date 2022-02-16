98824d092ce94d5d75207652c0022f61408375b1
mariohamameh <mariohamameh97@gmail.com>
2022-02-16 19:49:31 +0000

    added stuff

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,6 +3,13 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
+// test horizontally x
+// test vertically x
+// return false if not found x
+// return false if word is undefined 
+// return false if letters is empty array
+
+
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
@@ -20,7 +27,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +41,40 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['W', 'E', 'I', 'N', 'H', 'E', 'Q', 'D'],
+      ['Y', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the word is undefined", function() {
+    const result = wordSearch([
+      ['A', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['W', 'E', 'I', 'N', 'H', 'E', 'Q', 'D'],
+      ['Y', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], undefined)
+
+    assert.isFalse(result);
+  });
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'SEINFELD')
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
+const transpose = function(matrix) {
+    let newArr = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      newArr.push(matrix.map(m => m[i]))
+    };
+    return(newArr)
+};
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+        if (l.includes(word)) return true;
+
+const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    };
+
+};
+
+return false
+};
+
 
 module.exports = wordSearch
